2f95801a932abd4156e62d4ac62f099011104702
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2018-07-31 17:20:12 -0300

    Marca como pago

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -1,18 +1,34 @@
 from django.contrib import admin
 from django.utils.timezone import now
-
 from eventex.subscriptions.models import Subscription
 
+
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('name','email','phone', 'cpf', 'created_at','subscribed_today','paid')
-    date_hierarchy = 'created_at'
-    search_fields = ('name','email','phone','cpf')
-    list_filter = ('created_at','paid')
+	list_display = ('name', 'email', 'phone', 'cpf', 'created_at',
+		            'subscribed_today', 'paid')
+	date_hierarchy = 'created_at'
+	search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
+	list_filter = ('paid',  'created_at')
 
-    def subscribed_today(self, obj):
-        return obj.created_at == now().date()
+	actions = ['mark_as_paid']
 
-    subscribed_today.short_description = 'Inscrito hoje?'
-    subscribed_today.boolean = True
+	def subscribed_today(self, obj):
+		return obj.created_at.date() == now().date()
+
+	subscribed_today.short_description = 'inscrito hoje?'
+	subscribed_today.boolean = True
+
+	def mark_as_paid(self, request, queryset):
+		count = queryset.update(paid=True)
+
+		if count == 1:
+			msg = '{} inscrição foi marcada como paga.'
+		else:
+			msg = '{} inscrições forma marcadas como pagas.'
+
+		self.message_user(request, msg.format(count))
+
+	mark_as_paid.short_description = 'Marcar como pago'
+
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
