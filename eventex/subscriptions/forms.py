076f550398834def7cbbb2c5b968dabbb2fb17af
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2018-09-17 18:56:09 -0300

    Padroniza nomes e acertar conflitos

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-<<<<<<< HEAD
+from eventex.subscriptions.models import Subscription
+
 
 def validate_cpf(value):
     if not value.isdigit():
@@ -9,36 +10,18 @@ def validate_cpf(value):
     if len(value) != 11:
         raise ValidationError('CPF deve conter onze números')
 
-class SubscriptionForm(forms.Form):
-    name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
-=======
-from eventex.subscriptions.models import Subscription
-
-'''
-class SubscriptionFormOld(forms.Form):
-    name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email', required=False)
-    phone = forms.CharField(label='Telefone', required=False)
-
-'''
 
 class SubscriptionForm(forms.ModelForm):
 
     class Meta:
         model = Subscription
         fields = ['name','cpf','email','phone']
->>>>>>> 0c1e7745507a033ee2e39b332021a4795a39a8bf
 
     def clean_name(self):
         name = self.cleaned_data['name']
         words = [w.capitalize() for w in name.split()]
-<<<<<<< HEAD
-        return ' '.join(words)
-=======
+
+
         return ' '.join(words)
 
     def clean(self):
@@ -47,4 +30,3 @@ class SubscriptionForm(forms.ModelForm):
             raise ValidationError('Informe seu e-mail ou telefone')
 
         return self.cleaned_data
->>>>>>> 0c1e7745507a033ee2e39b332021a4795a39a8bf
